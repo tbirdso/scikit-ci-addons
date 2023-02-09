4b52ba097fe3bbd80a22d3a6db1554b119b24ef1
Tom Birdsong <tom.birdsong@kitware.com>
2023-02-09 15:28:19 -0500

    WIP: Use local updates to install_utils.ps1

--- windows/install-python.ps1
+++ windows/install-python.ps1
@@ -21,7 +21,7 @@ New-Item -ItemType Directory -Force -Path $downloadDir;
 
 if (![System.IO.File]::Exists("$downloadDir\install-utils.ps1")) {
   Write-Host "Download install-utils.ps1"
-  $url = "https://raw.githubusercontent.com/scikit-build/scikit-ci-addons/master/windows/install-utils.ps1"
+  $url = "https://raw.githubusercontent.com/tbirdso/scikit-ci-addons/install-utils-deprecate-ssl/windows/install-utils.ps1"
   (new-object net.webclient).DownloadFile($url, "$downloadDir\install-utils.ps1")
 }
 Import-Module "$downloadDir\install-utils.ps1" -Force
